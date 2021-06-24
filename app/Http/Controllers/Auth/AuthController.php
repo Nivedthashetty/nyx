50570d5dee2213fd4bcd2162b3977fd0aaa83d6c
Nivedthashetty <79963723+Nivedthashetty@users.noreply.github.com>
2021-06-24 14:56:07 +0530

    Add files via upload

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,146 @@
+<?php
+  
+namespace App\Http\Controllers\Auth;
+  
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+use App\Models\Appsetting;
+use Hash;
+  
+class AuthController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index()
+    {
+        return view('auth.login');
+    }  
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+    public function registration1()
+    {
+        return view('auth.registration1');
+    }
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+   
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('You have Successfully loggedin');
+        }
+  
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postRegistration(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+
+    public function postRegistration1(Request $request)
+    {  
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required',
+        ]);
+           
+        $data1 = $request->all();
+        $check = $this->create1($data1);
+         
+        return redirect("dashboard")->withSuccess('Great! You have Successfully added image');
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }
+
+     /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create1(array $data1)
+    {
+      return Appsetting::create([
+        'title' => $data1['title'],
+        'description' => $data1['description'],
+        'image' => $data1['image']
+      ]);
+    }
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
+    }
+}
